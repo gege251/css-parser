b399e452862dea8f2d22d122d5518bfc010ea350
Gergo <gege251@gmail.com>
2018-09-20 13:27:55 +0900

    [add] fixed tests

--- test/GrepSpec.hs
+++ test/GrepSpec.hs
@@ -4,16 +4,22 @@ module GrepSpec where
 
 import           Data.ByteString.Char8
 import           Grep
+import           Selector (Selector(..))
 import           Test.Hspec
 
 
 spec :: Spec
-spec =
-    describe "grep in string" $ do
-        it "should find class" $
-            grep "test-class" ("<div class=\"test-class\">Hello World</div>" :: ByteString)
-                `shouldBe` [1]
+spec = describe "grep in string" $ do
+    it "should find class" $ 
+        grep
+            (ClassSelector "test-class")
+            ("<div class=\"test-class\">Hello World</div>" :: ByteString)
+        `shouldBe`
+            [1]
 
-        it "should find multiple classes" $
-            grep "test-class" ("<div class=\"test-class\">\nHello World\n<span class=\"test-class2\"</span></div>" :: ByteString)
-                `shouldBe` [1, 3]
+    it "should find multiple classes" $
+        grep
+            (ClassSelector "test-class")
+            ("<div class=\"test-class\">\nHello World\n<span class=\"test-class2\"</span></div>" :: ByteString)
+        `shouldBe`
+            [1, 3]
--- test/LibSpec.hs
+++ test/LibSpec.hs
@@ -5,6 +5,7 @@ module LibSpec where
 import           Data.ByteString.Char8
 import           Lib
 import           Test.Hspec
+import           Selector (Selector(..))
 
 
 spec :: Spec
@@ -12,15 +13,15 @@ spec =
     describe "Lib functions" $ do
         it "should transpose grep results to a printable form (group by query)" $
             transposeGrepResults
-                [ ("file1", [ ("test-class1", [1,11,22]) ])
-                , ("file2", [ ("test-class1", [11,110]) ])
+                [ ("file1", [ (ClassSelector "test-class1", [1,11,22]) ])
+                , ("file2", [ (ClassSelector "test-class1", [11,110]) ])
                 ]
             `shouldBe`
-                [ ("test-class1", [ ("file1", [1,11,22]), ("file2", [11,110]) ] )]
+                [ (ClassSelector "test-class1", [ ("file1", [1,11,22]), ("file2", [11,110]) ] )]
 
         it "should filter empty files from results" $
             filterResults
-                [ ("test-class1", [ ("file1", [1,11,22]), ("file2", []) ] )]
+                False [ (ClassSelector "test-class1", [ ("file1", [1,11,22]), ("file2", []) ] )]
             `shouldBe`
-                [ ("test-class1", [ ("file1", [1,11,22])] )]
+                [ (ClassSelector "test-class1", [ ("file1", [1,11,22])] )]
 
